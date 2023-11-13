fdc2c319b45bb8ce60e04ddb4ad8aa779fbca2a8
kamuikatsurgi <shahkrishang11@gmail.com>
2023-11-13 12:37:39 +0530

    zkEVM configurations

---
+++ contracts/contracts/zkPCards.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract zkPCard is ERC721, ERC721Pausable, Ownable, ERC721Burnable {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("zkPCard", "ZKPC") Ownable(msg.sender) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) internal {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+}
--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.21",
+  networks: {
+    zkEVM: {
+      url: `https://rpc.public.zkevm-test.net`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
