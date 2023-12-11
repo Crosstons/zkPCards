fc28c95afcc9bb14317c9a7039de9b6540e98d34
kamuikatsurgi <shahkrishang11@gmail.com>
2023-12-11 11:47:19 +0530

    changed abis

--- contracts/contracts/Debit Card/DebitCard.sol
+++ contracts/contracts/Debit Card/DebitCard.sol
@@ -115,6 +115,7 @@ contract DebitCard is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
 
     function withdrawFunds(uint256 amount) external onlyOwner {
         require(amount <= address(this).balance, "Not enough funds in the contract!");
+        poolSize += amount;
         payable(owner()).transfer(amount);
     }
 
